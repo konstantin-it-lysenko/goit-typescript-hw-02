bff230b6dfc6c3a2155d7db2c16b3153903eaefc
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-01-05 22:45:32 +0200

    add union and literal types

--- src/baseTypes/5.ts
+++ src/baseTypes/5.ts
@@ -1,7 +1,5 @@
-/* 
-  Як ви визначите змінну в TypeScript, яка може приймати рядок або число (union type)? 
-  І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
-*/
+type DisableOrEnable = "enable" | "disable";
 
-let union;
-let literal;
+let union: string | number;
+let literal: DisableOrEnable;
+// let literal: "enable" | "disable";
